ac1701218947d20dd9dc7255a274a67bcea2ea53
Aftab Ahmad <aftab.era@gmail.com>
2024-02-25 18:43:30 +0500

    Cleanup Solution

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,2 @@
 ﻿
-using LogUtility;
-
-var logger = new LogEntry();
-logger.Log("Application started ...");
-logger.Log("Application stopping ...");
-
-SaveLogs.Save("logs.txt", logger.ToString());
-
-Console.WriteLine("Logs are created & Saved");
+Console.WriteLine("Hello World!");
