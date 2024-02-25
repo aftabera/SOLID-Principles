0ecfa0573c4dd77b34abeaebd8afeee5f0f77936
Aftab Ahmad <aftab.era@gmail.com>
2024-02-25 21:46:34 +0500

    SRP-Fixed

---
+++ ConsoleApp1/LogEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class LogEntry
+    {
+        public DateTime DateAndTimeForLog { get; set; }
+        public string LogMessage { get; set; }
+        
+        public override string ToString()
+        {
+            return $"{DateAndTimeForLog} - {LogMessage}";
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,10 @@
 ﻿
-Console.WriteLine("Hello World!");
+using ConsoleApp1;
+
+var logger = new LogsManager();
+logger.Log("Application is starting ...");
+logger.Log("Application is stopping ...");
+
+logger.SaveLogs("logs.txt");
+
+Console.WriteLine("Logs saved successfully!");
