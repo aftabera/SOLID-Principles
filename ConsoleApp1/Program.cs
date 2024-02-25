251a7cf46e5c0d45009b29ad0e62df1af794b40a
Aftab Ahmad <aftab.era@gmail.com>
2024-02-25 13:08:29 +0500

    SRP

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using LogUtility;
+
+var logger = new LogEntry();
+logger.Log("Application started ...");
+logger.Log("Application stopping ...");
+
+SaveLogs.Save("logs.txt", logger.ToString());
+
+Console.WriteLine("Logs are created & Saved");
---
+++ LogUtility/SaveLogs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogUtility
+{
+    public static class SaveLogs
+    {
+        public static void Save(string fileName, string logs)
+        {
+            File.WriteAllText(fileName, logs);
+        }
+    }
+}
